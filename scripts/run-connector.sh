650d315a6f3353f69490c651d751474f4af888ad
haziq <muhammadnurhaziq97@gmail.com>
2023-06-23 14:40:36 +0800

    update connector and create new container

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -1,2 +1,5 @@
-curl -i -X POST -H "Accept: application/json" -H "Content-Type:application/json"\
- localhost:8083/connectors/ --data-binary "@./SOURCE_DB2_DEBEZIUM_KAFKA.json"
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json"\
+ localhost:8083/connectors/ --data-binary "@./SOURCE_DB2_DEBEZIUM_KAFKA_POSTS.json"
+
+# curl -i -X PUT -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/example-db2-connector-v0.1/config --data-binary "@./SOURCE_DB2_DEBEZIUM_KAFKA.json"
+#  curl -i -X DELETE localhost:8083/connectors/example-db2-connector-posts-v0.1/
